b1da5b8989fb698dc03d6eb4cda31c3439cb95b4
mortality <kirillshamanaev1@gmail.com>
2024-10-31 00:16:57 +0500

    Ended with initDOMListeners and removeDOMListeners

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,6 +3,7 @@ import {DomListener} from "./DomListener"
 export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners || [])
+    this.name = options.name || ''
   }
 
   // return template of component
@@ -12,5 +13,9 @@ export class ExcelComponent extends DomListener {
 
   init() {
     this.initDOMListeners()
+  }
+
+  destroy() {
+    this.removeDOMListeners()
   }
 }
