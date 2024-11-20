56a691cac0b811f9ac9b1083ef411844e33262b2
mortality <kirillshamanaev1@gmail.com>
2024-11-21 01:20:49 +0500

    Completed toolbar and table heading
    The toolbar building has been completed, changing and saving the name of the table has been added

--- src/core/utils.js
+++ src/core/utils.js
@@ -1,4 +1,4 @@
-export function capitalize(string) { 
+export function capitalize(string) {
   if (typeof string !== 'string') {
     return ''
   }
@@ -29,4 +29,10 @@ export function isEqual(a, b) {
 
 export function camelToDashCase(str) {
   return str.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`);
+}
+
+export function toInlineStyles(styles = {}) {
+  return Object.keys(styles)
+      .map(key => `${camelToDashCase(key)}: ${styles[key]}`)
+      .join(';')
 }
